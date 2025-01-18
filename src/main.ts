93c0e2d93059b9e310dd390ec56cbd0a3fea20e8
JesusPM <jesus@perezdemiguel.com>
2025-01-18 10:23:41 +0100

    Installed and initialized GSAP library for tweens animations

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import gsap from 'gsap';
 import './global.css';
-import { Application } from 'pixi.js';
+import { Application, Container, Sprite } from 'pixi.js';
 
 (async () => {
   // Init Pixi.js
   const app = new Application();
   await app.init({ background: '#000000', resizeTo: window });
   document.body.appendChild(app.canvas);
+
+  // Init GSAP
+  gsap.registerPlugin(PixiPlugin);
+  PixiPlugin.registerPIXI({ Container, Sprite });
 
   // Add the main scene
 
