ee2db0c9774c6823609382b7136bd61de7b700b7
JesusPM <jesus@perezdemiguel.com>
2025-01-18 23:42:16 +0100

    Task 3: Done

--- src/scenes/task3/config.ts
+++ src/scenes/task3/config.ts
@@ -1,1 +1,11 @@
-
+export const TASK3_CONFIG = {
+  textLine: {
+    text: 'Press on the screen to change the fire location',
+    style: {
+      fontFamily: 'Arial',
+      fontSize: 46,
+      fill: 0xffffff,
+      align: 'center',
+    },
+  },
+};
--- src/scenes/task3/sceneTask3.ts
+++ src/scenes/task3/sceneTask3.ts
@@ -1,11 +1,20 @@
-import { Color, FederatedPointerEvent } from 'pixi.js';
+import {
+  Color,
+  FederatedPointerEvent,
+  Text,
+  TextStyle,
+  TextStyleOptions,
+} from 'pixi.js';
 import Scene from '../scene/scene';
 import { iSceneOptions } from '../scene/types';
 import { ParticleEmitter } from '../../systems/particles/ParticleEmitter';
 import { FIRE_PARTICLES_CONFIG } from '../../components/fire/configs/fireConfig';
+import FadeContainer from '../../components/fadeContainer/fadeContainer';
+import { TASK3_CONFIG } from './config';
 
 export default class SceneTask3 extends Scene {
   protected _fireEmitter!: ParticleEmitter;
+  protected _layerText!: FadeContainer;
 
   constructor(options: iSceneOptions) {
     super(options);
@@ -19,6 +28,11 @@ export default class SceneTask3 extends Scene {
     this._background.y = this.referenceFrame.height / 2;
 
     this.addChildAt(this._background, 0);
+
+    this._layerText = new FadeContainer(this.referenceFrame);
+    this._layerText.x = this.referenceFrame.width / 2;
+    this._layerText.y = (2 * this.referenceFrame.height) / 3;
+    this.addContent(this._layerText);
   }
 
   public async init() {
@@ -28,11 +42,17 @@ export default class SceneTask3 extends Scene {
     // Load the assets
     await this.load();
 
+    // Line text
+    const textStyle = new TextStyle(TASK3_CONFIG.textLine as TextStyleOptions);
+    const text = new Text(textStyle);
+    text.anchor.set(0.5);
+    this._layerText.addChild(text);
+
     // Create the particle emitter for the fire
     this._fireEmitter = new ParticleEmitter(FIRE_PARTICLES_CONFIG);
     this._fireEmitter.x = this.referenceFrame.width / 2;
     this._fireEmitter.y = this.referenceFrame.height / 2;
-    this.addChild(this._fireEmitter);
+    this.addContent(this._fireEmitter);
 
     // Set interaction for moving the fire location by pressing on the screen
     this._background.on('pointerdown', this._onBackgroundPress, this);
@@ -47,8 +67,11 @@ export default class SceneTask3 extends Scene {
 
   protected _onBackgroundPress(e: FederatedPointerEvent) {
     // Set the fire location to the pointer position
-    this._fireEmitter.x = e.client.x;
-    this._fireEmitter.y = e.client.y;
+    const localCoords = this.toLocal({ x: e.globalX, y: e.globalY });
+    this._fireEmitter.position.copyFrom(localCoords);
+
+    // Hide the text
+    this._layerText.hide();
   }
 
   public async _animate() {
